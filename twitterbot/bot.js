0f40b9aaffc3ddee8384164e1e14e476deea0a47
robbykraft <robbykraft@gmail.com>
2017-04-30 15:39:29 -0400

    bot is ready to go

--- twitterbot/bot.js
+++ twitterbot/bot.js
@@ -10,27 +10,31 @@ var config = require('./config');  //get keys from the other file
 // start twit with keys
 var T = new Twit(config);
 
+// 2 applications: 1 makes an animation of the next 6 hours into a .gif
+// var folderAnimationApp = 'application.macosx/';
+var folderAnimationApp = 'application.linux64/';
+var animationAppName = 'JovianMoons';
+// 1 makes a photo given a timestamp found in .txt file 'dateFile'
+// var folderPhotoApp = 'application.reader.macosx/';
+var folderPhotoApp = 'application.reader.linux64/';
+var photoAppName = 'JupiterMoons_reader';
+var dateFile = folderPhotoApp + 'date.txt';
+
 /////////////////////////////////////////////////////////
 ////////////////     TERMINAL     ///////////////////////
 /////////////////////////////////////////////////////////
 
 console.log("Starting Twitter Bot..");
 
-// var imagemakerFolder = 'application.macosx/';
-var imagemakerFolder = 'application.linux64/';
-
-// tweetForecastAnim();
-// setInterval(tweetForecastAnim, 1000 * 60 * 60 * 6);  // 6 hours
-
+tweetForecastAnim();
+setInterval(tweetForecastAnim, 1000 * 60 * 60 * 6);  // 6 hours
 
 function tweetForecastAnim(){
 	var startDate = moment().utc().format('D MMMM YYYY');
-	// var startTime = moment().format('YYYY-MM-DD hh:mm:ss a')
-	// var endTime = moment().add(6,'hours').format('YYYY-MM-DD hh:mm:ss a')
 	var startTime = moment().utc().format('H:mm');
 	var endTime = moment().utc().add(6,'hours').format('H:mm');
 
-	var tweetTextContent = ' ';
+	var tweetTextContent;
 	if(moment().utc().date() == moment().utc().add(6,'hours').date()){
 		tweetTextContent = startDate + "\nfrom " + startTime + " to " + endTime;
 	} else{
@@ -44,20 +48,20 @@ function tweetForecastAnim(){
 
 	// COMPILED PROCESSING SKETCH
 	// linux executable:
-	var cmd = '`pwd`/' + imagemakerFolder + 'JovianMoons';
+	var cmd = '`pwd`/' + folderAnimationApp + animationAppName;
 	// mac executable:
-	// var cmd = 'open ' + imagemakerFolder + 'JovianMoons.app';
+	// var cmd = 'open ' + folderAnimationApp + animationAppName + '.app';
 	exec(cmd, processingFinished);
 
 	function processingFinished(error, stdout, stderr){
 		if(error){ console.log(error); return; }
 
 		// convert -delay 3 -loop 0 *.png animated.gif
-		var cmd = 'convert -delay 3 -loop 0 ' + imagemakerFolder + 'images/*.png ' + imagemakerFolder + 'images/animated.gif';
+		var cmd = 'convert -delay 3 -loop 0 ' + folderAnimationApp + 'images/*.png ' + folderAnimationApp + 'images/animated.gif';
 		exec(cmd, uploadAndTweetImage);
 
 		function uploadAndTweetImage(){
-			var imageFile = imagemakerFolder + 'images/animated.gif';
+			var imageFile = folderAnimationApp + 'images/animated.gif';
 			var params = { encoding: 'base64' };
 			var b64 = fs.readFileSync(imageFile, params);
 			T.post('media/upload', { media_data: b64}, imageDidUpload);
@@ -83,38 +87,6 @@ function tweetForecastAnim(){
 		}
 	}
 }
-
-
-
-
-
-/*
-var exec = require('child_process').exec;
-var fs = require('fs');
-var cmd = '/Applications/LilyPond.app/Contents/Resources/bin/lilypond Untitled.ly';
-exec(cmd, processing);
-
-function processing() {
-	var filename = 'output.png';
-	var params = { encoding: 'base64' }
-	var b64 = fs.readFileSync(filename, params);
-	T.post('media/upload', { media_data: b64 }, uploaded);
-	function uploaded(err, data, response){
-		var id = data.media_id_string;
-		var tweet = {
-			status: 'tweet message',
-			media_ids: [id]
-		}
-		T.post('statuses/update', tweet, tweeted);
-		function tweeted(err, data, response){
-			if(err){
-				console.log("ERROR:");
-				console.log(err);
-			}
-			console.log(data);
-		}
-	}
-}*/
 
 /////////////////////////////////////////////////////////
 ////////////////      STREAM      ///////////////////////
@@ -143,30 +115,18 @@ function tweetAtMeEvent(eventMsg){
 	if(replyto === TWITTER_HANDLE){
 		console.log(from + ' sent us a tweet: "' + text + '"');
 		console.log(eventMsg);
+		// remove the @ mention of this twitter bot
 		var tweetContent = text.toLowerCase().replace('@'+TWITTER_HANDLE.toLowerCase(),'');
-		// tweetContent = tweetContent.replace('@'+TWITTER_HANDLE,'');
 		tweetContent = tweetContent.replace(' ', '');
-
-		// if(tweetContent === '' || tweetContent === ' '){
-			// console.log('Attepting IPA of ' + from);
-			// IPAFromWord(from, from, tweetReplyID);
-		// }
-		// else{
-			// console.log('Attepting IPA of ' + tweetContent);
-			// IPAFromWord(tweetContent, from, tweetReplyID);
-		// }
-		// var newTweet = '@' + from + ' ' + tweetContent;
 		tweetReplyTo(tweetContent, eventMsg);
 	}
 }
 
-var imagemakerReaderFolder = 'application.reader.linux64/';
-// var imagemakerReaderFolder = 'application.reader.macosx/';
-var dateFile = imagemakerReaderFolder + 'date.txt';
-
-
 function parseUserSubmission(textString){
 	var mDate = moment.unix(textString);//.utc();
+	if(mDate == undefined){
+		mDate = moment().utc();
+	}
 	var year = mDate.year();
 	var month = mDate.month() + 1;  // January start at 1
 	var day = mDate.date();
@@ -183,18 +143,18 @@ function parseUserSubmission(textString){
 function tweetReplyTo(tweetTextContent, eventMsg){
 	console.log("tweetReplyTo");
 	var tweet = {};
-	var imageFile = imagemakerReaderFolder + 'images/output.png';
+	var imageFile = folderPhotoApp + 'images/output.png';
+		// if(tweetContent === '' || tweetContent === ' '){
 
 	var dateString = parseUserSubmission(tweetTextContent);
 
 	// linux executable:
-	var cmd = '`pwd`/' + imagemakerReaderFolder + 'JupiterMoons_reader';
+	var cmd = '`pwd`/' + folderPhotoApp + photoAppName;
 	// mac executable:
-	// var cmd = 'open ' + imagemakerReaderFolder + 'JupiterMoons_reader.app';
+	// var cmd = 'open ' + folderPhotoApp + photoAppName + '.app';
 	exec(cmd, processingFinished);
 
 	function processingFinished(error, stdout, stderr){
-		console.log("processingFinished");
 		if(error){ console.log(error); return; }
 
 		var params = { encoding: 'base64' };
@@ -203,7 +163,6 @@ function tweetReplyTo(tweetTextContent, eventMsg){
 		T.post('media/upload', { media_data: b64}, imageDidUpload);
 
 		function imageDidUpload(err, data, response){
-			console.log("imageDidUpload");
 			// media has been uploaded, now we can tweet with the ID of the image
 			var id = data.media_id_string;
 			var replyTweetString = '@' + eventMsg.user.screen_name + ' ' + dateString;
@@ -216,7 +175,7 @@ function tweetReplyTo(tweetTextContent, eventMsg){
 			function tweeted(err, data, response) {
 				if(err){ console.log('ERROR: ' + err); }
 				else{
-					console.log('tweet posted');
+					console.log('reply posted');
 					fs.exists(imageFile, function(exists) {
 						if(exists) fs.unlink(imageFile);
 					});
@@ -228,8 +187,6 @@ function tweetReplyTo(tweetTextContent, eventMsg){
 		}
 	}
 }
-
-
 
 
 function tweetSimpleReplyTo(tweetTextContent, eventMsg){
@@ -249,44 +206,3 @@ function tweetSimpleReplyTo(tweetTextContent, eventMsg){
 		};
 	}
 }
-
-/////////////////////////////////////////////////////////
-////////////////      TWEET       ///////////////////////
-/////////////////////////////////////////////////////////
-/*
-tweetIt('my tweet message');
-
-setInterval(tweetIt('my tweet message'), 1000 * 60 * 5);
-
-function tweetIt(tweetText){
-	var tweet = { status: tweetText };
-
-	T.post('statuses/update', tweet, tweeted);
-
-	function tweeted(err, data, response){
-		if(err){
-			console.log("ERROR:");
-			console.log(err);
-		}
-		console.log(data);
-	}
-}
-*/
-/////////////////////////////////////////////////////////
-////////////////      SEARCH      ///////////////////////
-/////////////////////////////////////////////////////////
-/*
-var params = {
-	q: 'origami since:2011-07-11', 
-	count: 2 
-};
-
-T.get('search/tweets', params, gotData);
-
-function gotData(err, data, response) {
-	var tweets = data.statuses;
-	for(var i = 0; i < tweets.length; i++){
-		console.log(tweets[i].text);
-	}
-};
-*/
